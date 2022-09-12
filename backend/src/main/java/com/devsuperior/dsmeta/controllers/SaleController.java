1d27807959c0ccf1bc486df08c47a86a0b367224
elvis_tecnico_ti <elvistecnico_ti@protonmail.com>
2022-09-11 21:48:26 -0300

    Date update

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -28,7 +28,7 @@ public class SaleController {
 	public Page<Sale> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, Pageable pageable) {
 
-		return service.findSales(maxDate, maxDate, pageable);
+		return service.findSales(minDate, maxDate, pageable);
 	}
 
 	@GetMapping("/{id}/notification")
